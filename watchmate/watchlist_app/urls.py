5fc26743b0e52d6d8f75297f7273e68fdd5891d5
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-01-02 23:00:53 +0500

    basic django structure without rest

--- watchmate/watchlist_app/urls.py
+++ watchmate/watchlist_app/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path,include
-from watchlist_app.views import movieList
+from watchlist_app.views import movieList,movie_detail
 
 
 urlpatterns = [
-    path('list/',movieList,name="movie-list")
+    path('list/',movieList,name="movie-list"),
+    path('<int:pk>',movie_detail,name="movie-detail")
 ]
